4621f84fb85725ed833952dfbfdb746641366d9c
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-04-11 13:31:29 +0700

    Create bai1.sql

---
+++ baitap/day1/bai1.sql
@@ -0,0 +1,12 @@
+create database student_management;
+use student_management;
+create table class(
+id int,
+`name` varchar(50)
+);
+create table teacher(
+id int,
+`name` varchar(50),
+age int,
+coutry varchar(50)
+);
