5e9f507f274c40125673d93c2e53375cb78d78f5
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-04-15 13:57:17 +0700

    Update case_study.sql

--- baitap/case_study/case_study.sql
+++ baitap/case_study/case_study.sql
@@ -498,7 +498,15 @@ WHERE
 -- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm.
 --  Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, 
 -- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
-
+SELECT 
+hd.ma_hop_dong,
+hd.ngay_lam_hop_dong,
+hd.ngay_ket_thuc,
+hd.tien_dat_coc,
+SUM(hdct.so_luong) AS so_luong_dich_vu_di_kem
+FROM hop_dong hd 
+INNER JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+GROUP BY ma_hop_dong
 
 
 
