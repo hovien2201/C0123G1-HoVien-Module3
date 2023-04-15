ae203cac2dbe24b63da4ebf1e86981a70b89d9be
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-04-15 17:43:18 +0700

    Update case_study.sql

--- baitap/case_study/case_study.sql
+++ baitap/case_study/case_study.sql
@@ -506,9 +506,104 @@ hd.tien_dat_coc,
 SUM(hdct.so_luong) AS so_luong_dich_vu_di_kem
 FROM hop_dong hd 
 INNER JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
-GROUP BY ma_hop_dong
+GROUP BY ma_hop_dong;
 
+-- task 5 
+-- 11.	Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+SELECT dvdk.*
+FROM dich_vu_di_kem dvdk
+INNER JOIN hop_dong_chi_tiet hdct ON hdct.ma_dich_vu_di_kem=dvdk.ma_dich_vu_di_kem
+INNER JOIN hop_dong hd ON hd.ma_hop_dong=hdct.ma_hop_dong
+INNER JOIN khach_hang kh ON kh.ma_khach_hang= hd.ma_khach_hang
+INNER JOIN loai_khach lk ON lk.ten_loai_khach='Diamond'
+WHERE kh.dia_chi LIKE '%Vinh' OR kh.dia_chi LIKE '%Quảng Ngãi';
 
+-- 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng),
+--  so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), 
+-- tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
 
+SELECT 
+    hd.ma_hop_dong,
+    GROUP_CONCAT(nv.ho_ten) AS ho_ten_nhan_vien,
+    kh.ho_ten AS ho_ten_khach_hang,
+    kh.so_dien_thoai,
+    dv.ten_dich_vu,
+    SUM(hdct.so_luong) AS so_luong_dich_vu_di_kem,
+    SUM(hd.tien_dat_coc) AS tien_dat_coc
+FROM
+    hop_dong hd
+        INNER JOIN
+    khach_hang kh ON kh.ma_khach_hang = hd.ma_khach_hang
+        INNER JOIN
+    nhan_vien nv ON nv.ma_nhan_vien = hd.ma_nhan_vien
+        INNER JOIN
+    dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
+        INNER JOIN
+    hop_dong_chi_tiet hdct ON hdct.ma_hop_dong = hd.ma_hop_dong
+        INNER JOIN
+    dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+WHERE
+    YEAR(hd.ngay_lam_hop_dong) = '2020'
+        AND MONTH(hd.ngay_lam_hop_dong) > 9
+        AND hd.ma_dich_vu NOT IN (SELECT 
+            hd.ma_dich_vu
+        FROM
+            hop_dong hd
+                INNER JOIN
+            dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
+        WHERE
+            YEAR(hd.ngay_lam_hop_dong) = '2021'
+                AND MONTH(hd.ngay_lam_hop_dong) < 7)
+GROUP BY hd.ma_hop_dong
+;
 
--- DROP DATABASE furama;
+-- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.
+--  (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+SELECT 
+    dvdk.*
+FROM
+    dich_vu_di_kem dvdk
+        INNER JOIN
+    hop_dong_chi_tiet hdct ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+GROUP BY dvdk.ma_dich_vu_di_kem
+HAVING COUNT(dvdk.ma_dich_vu_di_kem) = (SELECT 
+        COUNT(hdct.ma_dich_vu_di_kem) AS so_lan
+    FROM
+        dich_vu_di_kem dvdk
+            INNER JOIN
+        hop_dong_chi_tiet hdct ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+    GROUP BY dvdk.ma_dich_vu_di_kem
+    ORDER BY so_lan DESC
+    LIMIT 1)
+;
+
+-- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
+--  Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung
+--  (được tính dựa trên việc count các ma_dich_vu_di_kem).
+SELECT 
+    dvdk.*
+FROM
+    dich_vu_di_kem dvdk
+        INNER JOIN
+    hop_dong_chi_tiet hdct ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+GROUP BY dvdk.ma_dich_vu_di_kem
+HAVING COUNT(dvdk.ma_dich_vu_di_kem) = 1 
+;
+
+-- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, 
+-- ten_bo_phan, so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+SELECT
+nv.ma_nhan_vien,
+nv.ho_ten,
+td.ten_trinh_do,
+bp.ten_bo_phan,
+nv.so_dien_thoai,
+nv.dia_chi
+FROM
+nhan_vien nv
+INNER JOIN hop_dong hd ON hd.ma_nhan_vien = nv.ma_nhan_vien
+INNER JOIN trinh_do td ON td.ma_trinh_do= nv.ma_trinh_do
+INNER JOIN bo_phan bp ON bp.ma_bo_phan=nv.ma_bo_phan
+WHERE YEAR(hd.ngay_lam_hop_dong)='2020' OR YEAR(hd.ngay_lam_hop_dong)='2021'
+GROUP BY nv.ma_nhan_vien
+HAVING COUNT(nv.ma_nhan_vien)<4;
