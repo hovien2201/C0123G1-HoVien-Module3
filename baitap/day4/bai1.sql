7e63408275ecc543274dd7a15ce74644bdff19fe
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-04-15 17:43:21 +0700

    Update bai1.sql

--- baitap/day4/bai1.sql
+++ baitap/day4/bai1.sql
@@ -25,15 +25,13 @@ WHERE s.credit=( SELECT max(credit) FROM subjects);
   
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
 SELECT 
-  s.*, 
-  m.mark 
-FROM 
-  subjects s 
-  INNER JOIN mark m 
-ORDER BY 
-  m.mark DESC 
-LIMIT 
-  1;
+    s.*, m.mark
+FROM
+    subjects s
+        INNER JOIN
+    mark m
+ORDER BY m.mark DESC
+LIMIT 1;
   
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 SELECT 
