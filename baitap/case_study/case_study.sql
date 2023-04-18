dc68050b32bd5ece9a46994cdb5bd1b9355f327d
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-04-18 14:31:35 +0700

    Update case_study.sql

--- baitap/case_study/case_study.sql
+++ baitap/case_study/case_study.sql
@@ -1,108 +1,120 @@
 CREATE DATABASE furama;
 USE furama;
 -- task 1,2
-CREATE TABLE vi_tri(
-  ma_vi_tri INT AUTO_INCREMENT, 
-  ten_vi_tri VARCHAR(45), 
-  PRIMARY KEY(ma_vi_tri)
+CREATE TABLE vi_tri (
+    ma_vi_tri INT AUTO_INCREMENT,
+    ten_vi_tri VARCHAR(45),
+    PRIMARY KEY (ma_vi_tri)
 );
 INSERT INTO vi_tri(ma_vi_tri, ten_vi_tri) VALUE(1, "thang");
-DELETE FROM 
-  vi_tri 
-WHERE 
-  ma_vi_tri = 1;
-CREATE TABLE trinh_do(
-  ma_trinh_do INT, 
-  ten_trinh_do VARCHAR(45), 
-  PRIMARY KEY(ma_trinh_do)
+DELETE FROM vi_tri 
+WHERE
+    ma_vi_tri = 1;
+CREATE TABLE trinh_do (
+    ma_trinh_do INT,
+    ten_trinh_do VARCHAR(45),
+    PRIMARY KEY (ma_trinh_do)
 );
-CREATE TABLE bo_phan(
-  ma_bo_phan INT, 
-  ten_bo_phan VARCHAR(45), 
-  PRIMARY KEY(ma_bo_phan)
+CREATE TABLE bo_phan (
+    ma_bo_phan INT,
+    ten_bo_phan VARCHAR(45),
+    PRIMARY KEY (ma_bo_phan)
 );
-CREATE TABLE nhan_vien(
-  ma_nhan_vien INT PRIMARY KEY, 
-  ho_ten VARCHAR(45) NOT NULL, 
-  ngay_sinh DATE NOT NULL, 
-  so_cmnd VARCHAR(45) NOT NULL UNIQUE, 
-  luong DOUBLE NOT NULL, 
-  so_dien_thoai VARCHAR(45) NOT NULL, 
-  email VARCHAR(45), 
-  dia_chi VARCHAR(45), 
-  ma_vi_tri INT, 
-  FOREIGN KEY(ma_vi_tri) REFERENCES vi_tri(ma_vi_tri), 
-  ma_trinh_do INT, 
-  FOREIGN KEY(ma_trinh_do) REFERENCES trinh_do(ma_trinh_do), 
-  ma_bo_phan INT, 
-  FOREIGN KEY(ma_bo_phan) REFERENCES bo_phan(ma_bo_phan)
+CREATE TABLE nhan_vien (
+    ma_nhan_vien INT PRIMARY KEY,
+    ho_ten VARCHAR(45) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    so_cmnd VARCHAR(45) NOT NULL UNIQUE,
+    luong DOUBLE NOT NULL,
+    so_dien_thoai VARCHAR(45) NOT NULL,
+    email VARCHAR(45),
+    dia_chi VARCHAR(45),
+    ma_vi_tri INT,
+    FOREIGN KEY (ma_vi_tri)
+        REFERENCES vi_tri (ma_vi_tri),
+    ma_trinh_do INT,
+    FOREIGN KEY (ma_trinh_do)
+        REFERENCES trinh_do (ma_trinh_do),
+    ma_bo_phan INT,
+    FOREIGN KEY (ma_bo_phan)
+        REFERENCES bo_phan (ma_bo_phan)
 );
-CREATE TABLE loai_khach(
-  ma_loai_khach INT PRIMARY KEY, 
-  ten_loai_khach VARCHAR(45) NOT NULL
+CREATE TABLE loai_khach (
+    ma_loai_khach INT PRIMARY KEY,
+    ten_loai_khach VARCHAR(45) NOT NULL
 );
-CREATE TABLE khach_hang(
-  ma_khach_hang INT PRIMARY KEY, 
-  ma_loai_khach INT, 
-  FOREIGN KEY(ma_loai_khach) REFERENCES loai_khach(ma_loai_khach), 
-  ho_ten VARCHAR(45) NOT NULL, 
-  ngay_sinh DATE NOT NULL, 
-  gioi_tinh BIT(1) NOT NULL, 
-  so_cmnd VARCHAR(45) NOT NULL, 
-  so_dien_thoai VARCHAR(45) NOT NULL, 
-  email VARCHAR(45), 
-  dia_chi VARCHAR(45)
+CREATE TABLE khach_hang (
+    ma_khach_hang INT PRIMARY KEY,
+    ma_loai_khach INT,
+    FOREIGN KEY (ma_loai_khach)
+        REFERENCES loai_khach (ma_loai_khach),
+    ho_ten VARCHAR(45) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    gioi_tinh BIT(1) NOT NULL,
+    so_cmnd VARCHAR(45) NOT NULL,
+    so_dien_thoai VARCHAR(45) NOT NULL,
+    email VARCHAR(45),
+    dia_chi VARCHAR(45)
 );
-CREATE TABLE dich_vu_di_kem(
-  ma_dich_vu_di_kem INT PRIMARY KEY, 
-  ten_dich_vu_di_kem VARCHAR(45) NOT NULL, 
-  gia DOUBLE, 
-  don_vi VARCHAR(45), 
-  trang_thai VARCHAR(45)
+CREATE TABLE dich_vu_di_kem (
+    ma_dich_vu_di_kem INT PRIMARY KEY,
+    ten_dich_vu_di_kem VARCHAR(45) NOT NULL,
+    gia DOUBLE,
+    don_vi VARCHAR(45),
+    trang_thai VARCHAR(45)
 );
-CREATE TABLE loai_dich_vu(
-  ma_loai_dich_vu INT PRIMARY KEY, 
-  ten_loai_dich_vu VARCHAR(45) NOT NULL
+CREATE TABLE loai_dich_vu (
+    ma_loai_dich_vu INT PRIMARY KEY,
+    ten_loai_dich_vu VARCHAR(45) NOT NULL
 );
-CREATE TABLE kieu_thue(
-  ma_kieu_thue INT PRIMARY KEY, 
-  ten_kieu_thue VARCHAR(45)
+CREATE TABLE kieu_thue (
+    ma_kieu_thue INT PRIMARY KEY,
+    ten_kieu_thue VARCHAR(45)
 );
-CREATE TABLE dich_vu(
-  ma_dich_vu INT PRIMARY KEY, 
-  ten_dich_vu VARCHAR(45) NOT NULL, 
-  dien_tich INT, 
-  chi_phi_thue DOUBLE NOT NULL, 
-  so_nguoi_toi_da INT, 
-  ma_kieu_thue INT, 
-  FOREIGN KEY(ma_kieu_thue) REFERENCES kieu_thue(ma_kieu_thue), 
-  ma_loai_dich_vu INT, 
-  FOREIGN KEY(ma_loai_dich_vu) REFERENCES loai_dich_vu(ma_loai_dich_vu), 
-  tieu_chuan_phong VARCHAR(45), 
-  mo_ta_tien_nghi_khac VARCHAR(45), 
-  dien_tich_ho_boi DOUBLE, 
-  so_tang INT, 
-  dich_vu_mien_phi_di_kem TEXT
+CREATE TABLE dich_vu (
+    ma_dich_vu INT PRIMARY KEY,
+    ten_dich_vu VARCHAR(45) NOT NULL,
+    dien_tich INT,
+    chi_phi_thue DOUBLE NOT NULL,
+    so_nguoi_toi_da INT,
+    ma_kieu_thue INT,
+    FOREIGN KEY (ma_kieu_thue)
+        REFERENCES kieu_thue (ma_kieu_thue),
+    ma_loai_dich_vu INT,
+    FOREIGN KEY (ma_loai_dich_vu)
+        REFERENCES loai_dich_vu (ma_loai_dich_vu),
+    tieu_chuan_phong VARCHAR(45),
+    mo_ta_tien_nghi_khac VARCHAR(45),
+    dien_tich_ho_boi DOUBLE,
+    so_tang INT,
+    dich_vu_mien_phi_di_kem TEXT
 );
-CREATE TABLE hop_dong(
-  ma_hop_dong INT PRIMARY KEY, 
-  ngay_lam_hop_dong DATETIME NOT NULL, 
-  ngay_ket_thuc DATETIME NOT NULL, 
-  tien_dat_coc DOUBLE NOT NULL, 
-  ma_nhan_vien INT, 
-  FOREIGN KEY(ma_nhan_vien) REFERENCES nhan_vien(ma_nhan_vien), 
-  ma_khach_hang INT, 
-  FOREIGN KEY(ma_khach_hang) REFERENCES khach_hang(ma_khach_hang), 
-  ma_dich_vu INT, 
-  FOREIGN KEY(ma_dich_vu) REFERENCES dich_vu(ma_dich_vu)
+CREATE TABLE hop_dong (
+    ma_hop_dong INT PRIMARY KEY,
+    ngay_lam_hop_dong DATETIME NOT NULL,
+    ngay_ket_thuc DATETIME NOT NULL,
+    tien_dat_coc DOUBLE NOT NULL,
+    ma_nhan_vien INT,
+    FOREIGN KEY (ma_nhan_vien)
+        REFERENCES nhan_vien (ma_nhan_vien)
+        ON DELETE SET NULL,
+    ma_khach_hang INT,
+    FOREIGN KEY (ma_khach_hang)
+        REFERENCES khach_hang (ma_khach_hang)
+        ON DELETE SET NULL,
+    ma_dich_vu INT,
+    FOREIGN KEY (ma_dich_vu)
+        REFERENCES dich_vu (ma_dich_vu)
 );
-CREATE TABLE hop_dong_chi_tiet(
-  ma_hop_dong_chi_tiet INT PRIMARY KEY, 
-  ma_hop_dong INT, 
-  FOREIGN KEY(ma_hop_dong) REFERENCES hop_dong(ma_hop_dong), 
-  ma_dich_vu_di_kem INT, 
-  FOREIGN KEY(ma_dich_vu_di_kem) REFERENCES dich_vu_di_kem(ma_dich_vu_di_kem), 
-  so_luong INT NOT NULL
+CREATE TABLE hop_dong_chi_tiet (
+    ma_hop_dong_chi_tiet INT PRIMARY KEY,
+    ma_hop_dong INT,
+    FOREIGN KEY (ma_hop_dong)
+        REFERENCES hop_dong (ma_hop_dong),
+    ma_dich_vu_di_kem INT,
+    FOREIGN KEY (ma_dich_vu_di_kem)
+        REFERENCES dich_vu_di_kem (ma_dich_vu_di_kem),
+    so_luong INT NOT NULL
 );
 -- thêm dữ liệu task3
 INSERT INTO VI_Tri VALUE(1, "LỄ Tân"), 
@@ -288,46 +300,16 @@ VALUES
     4, 3, 'nORmal', 'Có tivi', null, null, 
     '1 Xe máy'
   );
-INSERT INTO hop_dong VALUE(
-  1, "2023-11-4", "2023-11-6", 0, 3, 1, 
-  3
-), 
-(
-  2, "2023-1-4", "2023-1-6", 200000, 
-  7, 3, 1
-), 
-(
-  3, "2023-6-4", "2023-6-8", 500000, 
-  3, 4, 2
-), 
-(
-  4, "2023-1-12", "2023-1-16", 1000000, 
-  7, 5, 5
-), 
-(
-  5, "2021-11-9", "2021-11-14", 0, 7, 
-  2, 6
-), 
-(
-  6, "2021-1-8", "2021-1-12", 600000, 
-  7, 7, 6
-), 
-(
-  7, "2023-12-6", "2023-12-8", 700000, 
-  7, 4, 4
-), 
-(
-  8, "2023-3-1", "2023-3-2", 1000000, 
-  3, 4, 1
-), 
-(
-  9, "2023-11-4", "2023-11-8", 300000, 
-  3, 4, 3
-), 
-(
-  10, "2022-11-6", "2022-11-10", 50000, 
-  10, 3, 5
-);
+INSERT INTO hop_dong VALUE(1,"2023-11-4","2023-11-6",0,4,1,3),
+(2,"2020-1-4","2023-1-6",200000,5,3,1),
+(3,"2021-6-4","2023-6-8",500000,3,4,2),
+(4,"2022-1-12","2023-1-16",1000000,7,5,5),
+(5,"2021-11-9","2021-11-14",0,10,2,6),
+(6,"2019-1-8","2021-1-12",600000,7,7,6),
+(7,"2022-12-6","2023-12-8",700000,2,4,4),
+(8,"2020-3-1","2023-3-2",1000000,3,4,1),
+(9,"2023-11-4","2023-11-8",300000,8,4,3),
+(10,"2022-11-6","2022-11-10",50000,6,3,5);
 INSERT INTO hop_dong_chi_tiet VALUE(1, 2, 1, 5), 
 (2, 3, 2, 8), 
 (3, 2, 3, 7), 
@@ -650,16 +632,9 @@ GROUP BY nv.ma_nhan_vien
 HAVING COUNT(nv.ma_nhan_vien) < 4;
 
 -- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
--- DELIMITER //
--- CREATE TRIGGER trigger_nhan_vien
--- BEFORE DELETE ON nhan_vien
--- FOR EACH ROW
--- BEGIN
--- DELETE FROM hop_dong h WHERE h.ma_nhan_vien=old.ma_nhan_vien;
--- END //
--- DELIMITER ;
 
 SET SQL_SAFE_UPDATES=0;
+SET FOREIGN_KEY_CHECKS = 0; -- tat lien ket khoa ngoai
 
 DELETE FROM nhan_vien nv 
 WHERE
@@ -671,13 +646,8 @@ WHERE
         YEAR(hd.ngay_lam_hop_dong) = 2019
         OR YEAR(hd.ngay_lam_hop_dong) = 2020
         OR YEAR(hd.ngay_lam_hop_dong) = 2021) AS ma);
-        
+SET FOREIGN_KEY_CHECKS = 1;       
 SET SQL_SAFE_UPDATES=1;
-
-SELECT ma_nhan_vien FROM hop_dong hd WHERE YEAR(hd.ngay_lam_hop_dong )=2019 OR YEAR(hd.ngay_lam_hop_dong )=2020 OR YEAR(hd.ngay_lam_hop_dong )=2021;
-SELECT * FROM nhan_vien WHERE  exists (
-SELECT ma_nhan_vien FROM nhan_vien WHERE ma_nhan_vien NOT IN(
-SELECT ma_nhan_vien FROM hop_dong hd WHERE YEAR(hd.ngay_lam_hop_dong )=2019 OR YEAR(hd.ngay_lam_hop_dong )=2020 OR YEAR(hd.ngay_lam_hop_dong )=2021));
 
 
 -- 17.	Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum lên Diamond,
@@ -695,21 +665,16 @@ SET SQL_SAFE_UPDATES =1;
 
 -- 18.	Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc giữa các bảng).
 
-CREATE VIEW view_khach_hang AS (
-SELECT kh.ma_khach_hang FROM  khach_hang kh
-INNER JOIN hop_dong hd ON kh.ma_khach_hang=hd.ma_khach_hang
-WHERE  YEAR(hd.ngay_lam_hop_dong) <2021
-GROUP BY kh.ma_khach_hang
-);
-SELECT * FROM view_khach_hang;
-DELETE FROM view_khach_hang WHERE ma_khach_hang =1;
 SET SQL_SAFE_UPDATES =0;
+SET FOREIGN_KEY_CHECKS = 0;
 
-DELETE FROM khach_hang WHERE ma_khach_hang IN(SELECT kh.ma_khach_hang FROM  khach_hang kh
+DELETE FROM khach_hang WHERE ma_khach_hang IN( SELECT mkh FROM(SELECT kh.ma_khach_hang mkh FROM  khach_hang kh
 INNER JOIN hop_dong hd ON kh.ma_khach_hang=hd.ma_khach_hang
 WHERE  YEAR(hd.ngay_lam_hop_dong) <2021
-GROUP BY kh.ma_khach_hang);
+GROUP BY kh.ma_khach_hang) AS ma_kh);
 
+SET FOREIGN_KEY_CHECKS = 1;
+SET SQL_SAFE_UPDATES =1;
 -- 19.	Cập nhật giá cho các dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2020 lên gấp đôi.
 SET SQL_SAFE_UPDATES =0;
 UPDATE dich_vu_di_kem 
@@ -728,16 +693,7 @@ SET SQL_SAFE_UPDATES =1;
 -- thông tin hiển thị bao gồm id (ma_nhan_vien, ma_khach_hang), ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi.
 SELECT ma_nhan_vien AS ma ,ho_ten,email,so_dien_thoai,ngay_sinh,dia_chi FROM nhan_vien 
 UNION
-SELECT ma_khach_hang,ho_ten,email,so_dien_thoai,ngay_sinh,dia_chi FROM khach_hang kh
-
-
-
-
-
-
-
-
-
+SELECT ma_khach_hang,ho_ten,email,so_dien_thoai,ngay_sinh,dia_chi FROM khach_hang kh;
 
 
 
