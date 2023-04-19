436b4194870b21b26de94089ed24698056796574
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-04-19 19:34:26 +0700

    Create Main.java

---
+++ baitap/thi_modul2/src/bai3/view/Main.java
@@ -0,0 +1,10 @@
+package bai3.view;
+
+import bai3.controller.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.mainMenu();
+    }
+}
