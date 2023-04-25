297ac7c0f16c54c74cd986d6b605b1d405db1ce9
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-04-25 10:56:32 +0700

    Delete CheckRegex.java

--- baitap/thi_modul2/src/bai3/util/CheckRegex.java
+++
@@ -1,8 +0,0 @@
-package bai3.util;
-
-public class CheckRegex {
-    public static boolean checkMaHoaDon(String str) {
-        String regex = "^MHD-[0-9]{4}$";
-        return (str.matches(regex));
-    }
-}
