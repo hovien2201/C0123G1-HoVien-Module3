18db72c894a8839a29e70d05011510cf3262686a
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-04-27 15:35:46 +0700

    Create Calculator.java

---
+++ baitap/day13/bai2day13/bai2day13/src/main/java/bai_tap2/Calculator.java
@@ -0,0 +1,16 @@
+package bai_tap2;
+
+public class Calculator {
+    public static float addition(float a,float b){
+        return a+b;
+    }
+    public static float subtraction(float a,float b){
+        return a-b;
+    }
+    public static float multiplication(float a,float b){
+        return a*b;
+    }
+    public static float division(float a,float b){
+        return a/b;
+    }
+}
