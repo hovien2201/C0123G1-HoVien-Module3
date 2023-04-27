ff3b797ada5947f0f981a9920d7eae20de5b9eb7
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-04-27 15:36:44 +0700

    Create GiaiThuat.java

---
+++ baitap/giaithuat/GiaiThuat.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class GiaiThuat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap n");
+        int n = scanner.nextInt();
+        giaiThuat(n);
+    }
+
+    static void giaiThuat(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i < n/2) {
+                    if (j >= i && j < n - i) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                } else {
+                    if (j < n - i - 1 || j > i) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("*");
+                    }
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+}
